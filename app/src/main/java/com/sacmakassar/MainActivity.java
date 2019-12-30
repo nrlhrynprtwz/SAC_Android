c03aad86908f71df1f91a6d175fd514c250e05b3
nrlhrynprtwz <wiwitiwiyani@gmail.com>
2019-12-30 23:20:16 +0700

    Splashscreen
    tampilan awal aplikasi

--- app/src/main/java/com/sacmakassar/MainActivity.java
+++ app/src/main/java/com/sacmakassar/MainActivity.java
@@ -2,6 +2,7 @@ package com.sacmakassar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000); // Waktu loading 3 Detik
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                } finally {
+                    Intent login= new Intent(getApplicationContext(), login_costumer.class);
+                    startActivity(login);
+                    finish();
+                }
+            }
+        });
+        thread.start();
     }
 }
