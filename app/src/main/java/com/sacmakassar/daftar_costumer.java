f55dec346276c0e65b8b07a7e066be3593f33628
nrlhrynprtwz <wiwitiwiyani@gmail.com>
2020-01-01 17:49:08 +0700

    17.48 rabu

---
+++ app/src/main/java/com/sacmakassar/daftar_costumer.java
@@ -0,0 +1,14 @@
+package com.sacmakassar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class daftar_costumer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daftar_costumer);
+    }
+}
---
+++ app/src/main/java/com/sacmakassar/login_costumer.java
@@ -0,0 +1,14 @@
+package com.sacmakassar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login_costumer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_costumer);
+    }
+}
